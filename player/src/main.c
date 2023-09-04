1f761c1e0ee23fcc584df048daada219b1ef45fd
Conan Bitter <conan.bitter@gmail.com>
2023-09-04 21:00:45 +0300

    Add SDL scaffolding

--- player/src/main.c
+++ player/src/main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+SDL_Window *window;
+SDL_Renderer *renderer;
+
+int screen_width = 100, screen_height = 100, screen_scale = 1;
 
 void main() {
-    printf("Hello");
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("RVF", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_width, screen_height, SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    SDL_Event event;
+    int working = 1;
+    while (working) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    working = 0;
+                    break;
+            }
+        }
+        SDL_Delay(5);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
